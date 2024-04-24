2a1d288b3009e642785d9d9a8930c997d56af88d
awalch <alexander.walch@profactor.at>
2024-04-24 21:44:21 +0200

    changing the string descriptor

--- src/main_usbInput.cpp
+++ src/main_usbInput.cpp
@@ -1,6 +1,25 @@
 #include <plotter.h>
 #include <Audio.h>
 
+#define AUDIO_kHz ((int) AUDIO_SAMPLE_RATE / 1000)
+#define AUDIO_CHANNELS USB_AUDIO_NO_CHANNELS_480
+
+// Changing string in descriptor keeps Windows 10 happier
+extern "C"
+{
+    struct usb_string_descriptor_struct
+    {
+        uint8_t bLength;
+        uint8_t bDescriptorType;
+        uint16_t wString[6+1+1+2+1];
+    };
+
+  usb_string_descriptor_struct usb_string_serial_number={
+    2+(6+1+1+2+1)*2,3,
+    {'A','u','d','i','o','-','0'+AUDIO_CHANNELS,'/','0'+(AUDIO_kHz / 10),'0' + (AUDIO_kHz % 10),'B'}
+  };
+}
+
 //#include <util/usbHostRequestBuffer.h>  only used for debugging the initialization
 
 //activate one of the four options
